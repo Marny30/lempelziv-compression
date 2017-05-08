5c59c44e700a0e61b753a08d48eba1937d49e3af
Guilhem BLANCHARD <guilhem@debian>
2017-05-08 09:56:53 +0200

    correction graphe et extention

--- comp.sh
+++ comp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp ./projet/$1.c ./projet/$1.LZ.c
-cp ./projet/$1.c ./projet/$1.XZ.c
-cp ./projet/$1.c ./projet/$1.BZ.c
-cp ./projet/$1.c ./projet/$1.ZIP.c
+cp ./projet/$1 ./projet/$1.LZ
+cp ./projet/$1 ./projet/$1.XZ
+cp ./projet/$1 ./projet/$1.BZ
+cp ./projet/$1 ./projet/$1.ZIP
 echo "\documentclass[10pt,a4paper]{article}"
 echo "\usepackage{tikz}"
 echo "\usepackage{pgfplots}"
@@ -10,11 +10,17 @@ echo "\begin{document}"
 echo "\begin{tikzpicture}[scale=0.7]"
 echo "\begin{axis}[ybar,xticklabels={0,0,lzma,xz,bzip2,zip,huffman}]"
 
-MyLZMA=$({ time lzma -z ./projet/$1.LZ.c; } 2>&1 | grep real| sed -e 's/real\t0m//'| sed -e 's/s//')
-MyXZ=$({ time xz -z ./projet/$1.XZ.c; } 2>&1 | grep real| sed -e 's/real\t0m//'| sed -e 's/s//')
-MyBZIP2=$({ time  bzip2 -z ./projet/$1.BZ.c; } 2>&1 | grep real| sed -e 's/real\t0m//'| sed -e 's/s//')
-MyZIP=$({ time  zip -q -r ./projet/$1.ZIP.c.zip ./projet/$1.ZIP.c; } 2>&1 | grep real| sed -e 's/real\t0m//'| sed -e 's/s//')
-echo "\addplot coordinates{(1,"$MyLZMA")(2,"$MyXZ")(3,"$MyBZIP2")(4,"$MyZIP")(5,0.025)};"
+MyLZMA=$({ time lzma -z ./projet/$1.LZ; } 2>&1 | grep real| sed -e 's/real\t//'| sed -e 's/s//')
+MyXZ=$({ time xz -z ./projet/$1.XZ; } 2>&1 | grep real| sed -e 's/real\t//'| sed -e 's/s//')
+MyBZIP2=$({ time  bzip2 -z ./projet/$1.BZ; } 2>&1 | grep real| sed -e 's/real\t//'| sed -e 's/s//')
+MyZIP=$({ time  zip -q -r ./projet/$1.ZIP.zip ./projet/$1.ZIP; } 2>&1 | grep real| sed -e 's/real\t//'| sed -e 's/s//')
+
+LZMA=$(./comp.py $MyLZMA)
+XZ=$(./comp.py $MyXZ)
+BZIP2=$(./comp.py $MyBZIP2)
+ZIP=$(./comp.py $MyZIP)
+
+echo "\addplot coordinates{(1,"$LZMA")(2,"$XZ")(3,"$BZIP2")(4,"$ZIP")(5,0.025)};"
 
 echo "\end{axis}"
 echo "\end{tikzpicture}"
